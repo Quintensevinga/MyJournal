378f5c45b9d48f20d797ab180254a4e23855cda6
Quintensevinga <quintensevinga@outlook.com>
2023-09-27 17:12:51 +0200

    update server

--- my-journal/server/index.js
+++ my-journal/server/index.js
@@ -2,16 +2,26 @@ const Koa = require('koa');
 const router = require('./router');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
+const https = require('https'); 
+const fs = require('fs'); 
 
 const app = new Koa();
 
 app.use(cors());
 app.use(bodyParser());
-app.use(router.routes())
+app.use(router.routes());
 
-const PORT = 3001;
+const PORT = 443;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+const privateKey = fs.readFileSync('../private-key.pem', 'utf8');
+const certificate = fs.readFileSync('../certificate.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+
+const callback = app.callback(); 
+
+const httpsServer = https.createServer(credentials, callback); 
+
+httpsServer.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT} (HTTPS)`);
 });
-
